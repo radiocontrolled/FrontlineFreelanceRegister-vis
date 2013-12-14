0f3d293a3a139a49f4a909ee7c766a5c9525773e
radiocontrolled <radiocontrolled@gmail.com>
2013-12-14 11:47:06 +0000

    cleaning up code

--- ffr.js
+++ ffr.js
@@ -20,56 +20,38 @@ d3.csv("FFR_skill.csv", function(data){
 	var counts = _.pluck(data, 'Count')
 	counts = $.map(counts,Number);
 		
-	// outer radius of the pie is half the width
 	var outerRadius = w / 5;
 
 	//draw wedges
 	var arc = d3.svg.arc()
 		.innerRadius(100) 
 		.outerRadius(outerRadius);
+		
 	
-
+	
 	// define default pie layout
 	var pie = d3.layout.pie();
+
 	
 	var arcs = svg.selectAll("g.arc") 
 		.data(pie(counts)) 
 		.enter()
 		.append("g")
 		.attr("transform", "translate(" + outerRadius + "," + outerRadius + ")"); 	
-		
+
+
 	//Draw arc paths	
 	arcs.append("path")
 		.attr("fill", "#2CA266")
 		.attr("d", arc)
-		.on("mouseover",function(d,i){				
-			d3.select(this).classed("highlight",true)
-			d3.select("#tooltip")
-				.classed("hidden",false)
-				.select("#skill")		
-				.append("text")
-				.classed("hidden",false)
-				.text(function(){
-					//find & return the label 
-					for(var i = 0; i < data.length; i++){
-						data[i].Count = parseInt(data[i].Count);
-						if(data[i].Count == d.value){
-							return data[i].Skill;
-						}
-					}				
-			})
-		})
-		.on("mouseout", function(d,i){
-			d3.select(this).classed("highlight",false);
-			d3.select("#tooltip")
-				.classed("hidden",true)	
-			d3.select("#skill text").remove();
-		})	
 		
+			
+
 	//label 
 	arcs.append("text")
-		.attr("transform", function(d) {
-			return "translate(" + arc.centroid(d) + ")";
+		.attr("transform", function(d,i) {
+		// return "translate(" + arc.centroid(d) + ")";
+			return "translate(" + arc.centroid(d) + ")"; 	
 		})
 		.attr({
 			"text-anchor":"middle",
@@ -79,5 +61,39 @@ d3.csv("FFR_skill.csv", function(data){
 		.text(function(d) {
 			return d.value;
 		})
+	
+	svg.append("div")
+		.attr("id", "tooltip")
+		.append("p").attr("id", "skill")
+		.append("text")
+	
+	d3.selectAll("path")
+		.on("mouseover",function(d,i){				
+			d3.select(this).classed("highlight",true)
+			d3.select("#tooltip")
+				.classed("hidden",false)
+				.select("#skill")		
+				.append("text")
+				.text(function(){
+					//find & return the label 
+					for(var i = 0; i < data.length; i++){
+						data[i].Count = parseInt(data[i].Count);
+						if(data[i].Count == d.value){
+							return data[i].Skill;
+						}
+					}				
+				})	
+			})
+		.on("mouseout", function(d,i){
+		d3.select(this).classed("highlight",false);
+			d3.select("#tooltip")
+				.classed("hidden",true)	
+			d3.select("#skill text").remove();
+			
+		})
+
+
 })
+
+
 
